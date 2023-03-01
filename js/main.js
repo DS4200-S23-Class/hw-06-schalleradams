fdc06772873e82c054a83ccd738192ce33c599c0
schallerv <schaller.v@northeastern.edu>
2023-02-28 20:50:14 -0500

    left link

--- js/main.js
+++ js/main.js
@@ -114,56 +114,6 @@ function build_plots() {
         .call(d3.axisLeft(WidthYScale).ticks(4)) 
           .attr("font-size", '10px'); 
 
-    
-    d3.select("#width")
-          .call( d3.brush()                     // Add the brush feature using the d3.brush function
-            .extent( [ [0,0], [(VIS_WIDTH + MARGINS.right), (VIS_HEIGHT + MARGINS.top)] ] )       // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-            .on("start brush", updateChart)
-          )
-
-
-    // Function that is triggered when brushing is performed
-    function updateChart(event) {
-      extent = event.selection  //get coordinate
-      wpoints.classed("selected", function(d){ return isBrushed(extent, X_SCALE(d.Sepal_Width), Y_SCALE(d.Petal_Width) ) } )
-      linkChart
-    }
-
-    // A function that return TRUE or FALSE according if a dot is in the selection or not
-    function isBrushed(brush_coords, cx, cy) {
-      let x = (cx + MARGINS.left);
-      let y = (cy + MARGINS.top);
-      var x0 = brush_coords[0][0],
-          x1 = brush_coords[1][0],
-          y0 = brush_coords[0][1],
-          y1 = brush_coords[1][1];
-        return x0 <= x && x <= x1 && y0 <= y && y <= y1;    // This return TRUE or FALSE depending on if the points is in the selected area
-    }
-  }); 
-}
-
-
-// somehow do linking here maybe??
-function linkChart() {
-  d3.csv("data/iris.csv").then((data) => {
-    points.classed("selected", function(d){ return isSelected(d.id) } )
-    });
-}
-
-// return true if the point on the length scatter is sleected
-function isSelected(id) {
-  let WidthID = "W" + id
-  let LPoint = document.getElementById(WidthID)
-
-  return LPoint.classList.contains("selected");
-}
-
-
-build_scatters();
-
-// read data and create  bar chart
-d3.csv("data/iris.csv").then((data) => {
-
     // scale for X
     const BarXScale = d3.scaleBand() 
                       .domain(["virginica", "versicolor", "setosa"]) 
